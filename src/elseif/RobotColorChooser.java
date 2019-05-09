b42be4db1d5873136fe2613a411b4a13e48b6449
ilabstaff <ilabstaff@172.16.116.67>
2019-05-08 18:01:41 -0700

    were on robot

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -8,9 +8,12 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 import java.awt.Color;
+import java.security.SecurityPermission;
 
 
 public class RobotColorChooser {
+	private static Color red;
+
 	public static void main(String[] args) {
 		//1. Create a new Robot
 		Robot spot=new Robot();
@@ -20,16 +23,19 @@ public class RobotColorChooser {
 		String color=JOptionPane.showInputDialog("what color d you want");
 		//5. Use an if/else statement to set the pen color that the user requested
    if(color.equals("red")) {
+	   spot.setPenColor(red);
+	
+   
+        //7. Put a loop around your code so that you 
 	   
    }
-        //6. If the user doesn’t enter anything, choose a random color
-
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+        //6. If the user doesn’t enter anything, choosekeep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		
+		spot.setPenWidth(10);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
+spot.penDown();
+		spot.move(100);
 
 	}
 }
