70c5e79eda53440928879b05bdd1d99be2d205e0
ilabstaff <ilabstaff@172.16.116.81>
2019-05-15 18:00:07 -0700

    bananna quiz

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -12,9 +12,10 @@ import java.security.SecurityPermission;
 
 
 public class RobotColorChooser {
-	private static Color red;
+	
 
 	public static void main(String[] args) {
+		for(int i =0;i<100;i++) {
 		//1. Create a new Robot
 		Robot spot=new Robot();
 		
@@ -23,19 +24,46 @@ public class RobotColorChooser {
 		String color=JOptionPane.showInputDialog("what color d you want");
 		//5. Use an if/else statement to set the pen color that the user requested
    if(color.equals("red")) {
-	   spot.setPenColor(red);
-	
-   
-        //7. Put a loop around your code so that you 
+	   drawsquare(Color.red);
+//	   spot.setPenColor(Color.red);
+//		spot.setPenWidth(100);
+//		spot.penDown();
+//		spot.setAngle(90);
+//		spot.move(300);
+       break;
+		
+		
+		//7. Put a loop around your code so that you 
 	   
+   }else if(color.equals("blue")){
+	   drawsquare(Color.blue);  
    }
         //6. If the user doesn’t enter anything, choosekeep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		spot.setPenWidth(10);
+	
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-spot.penDown();
-		spot.move(100);
+		
+	}
 
 	}
+static void drawsquare(Color color) {
+	Robot spot=new Robot();
+	spot.setPenColor(Color.red);
+    spot.setSpeed(10);
+	spot.penDown();
+	spot.move(30);
+	spot.setAngle(30);
+	spot.move(200);
+   spot.setAngle(60);	
+   spot.move(200);
+   spot.setAngle(120);
+   spot.move(200);
+   spot.setAngle(240);
+   spot.move(200);
+   spot.setAngle(290);
+   spot.move(-300);
+   spot.setAngle(90);
+   spot.move(500);
+}  
 }
--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -10,15 +10,18 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String objects =JOptionPane.showInputDialog("do you like banannas");
 		//2. if they say no, 
-			//tell them they are crazy 
-			//and end quiz
+			if(objects.equals("no"))
+//		/tell them they are crazy 
+			JOptionPane.showMessageDialog(null, "your crazy");
+				//and end quiz
 		//3. if they say yes
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
 
-		//4. OPTIONAL: if they say something other than “yes�? or “no�?
-		//	show a pop up that says “You are bananas!�?
+		//4. OPTIONAL: if they say something other than “yes�? or “no�?
+		//	show a pop up that says “You are bananas!�?
 	
 	}
 
