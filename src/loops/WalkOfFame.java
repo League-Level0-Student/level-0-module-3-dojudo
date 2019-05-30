77d43424fd165d9f51f41e3af29d4d1a0f259a38
ilabstaff <ilabstaff@172.16.116.81>
2019-05-29 17:14:53 -0700

    done with this module

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -8,13 +8,31 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
+		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
 
-		// 2. Make the robot draw a star shape. Hint: 144.
-
-		// 3. Set the size of the star to 30.
+	// 2. Make the robot draw a star shape. Hint: 144.
+		Robot rob = new Robot();
+		rob.setX(100);
+		rob.setY(250);
+	int y =100;
+	int x = 250;
+	
+	for(int i=0;i<10;i++) {	
+		
+		rob.penDown();
+		rob.setSpeed(20);
+			
+		
+		for(int j=0;j<5;j++) {
+			
+			rob.move(150);
+		    rob.turn(144);
+		
+		}
+		rob.setX(rob.getX()+150);
+	}		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
